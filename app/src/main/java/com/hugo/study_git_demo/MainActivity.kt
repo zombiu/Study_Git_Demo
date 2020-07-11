6b595521d41e57bee5446bac72737c0a578bb503
zomubi <zomubi@163.com>
2020-07-11 10:59:02 +0800

    1.分支2.2的修改。

--- app/src/main/java/com/hugo/study_git_demo/MainActivity.kt
+++ app/src/main/java/com/hugo/study_git_demo/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * 2.2修改
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
